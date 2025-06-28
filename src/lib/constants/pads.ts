37dab4bce1edc2179b47a80fbf0c01d682d18d42
Marcus Gajozo <marcusgajozo@gmail.com>
2025-06-28 12:56:52 -0400

    refactor(constants): update constant pads

--- src/lib/constants/pads.ts
+++ src/lib/constants/pads.ts
@@ -1,5 +1,16 @@
-import padA from "@/audios/pads/pad-a.mp3";
+import padA from "@audios/pads/pad-a.mp3";
 
 export const padsContinuos = {
+  C: "",
+  "C#": "",
+  D: "",
+  "D#": "",
+  E: "",
+  F: "",
+  "F#": "",
+  G: "",
+  "G#": "",
   A: padA,
+  "A#": "",
+  B: "",
 } as const;
