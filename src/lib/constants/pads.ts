dbb4f8435f9dfffdbf8a555f8c1f7ccd1b71a330
Marcus Gajozo <marcusgajozo@gmail.com>
2025-07-03 12:11:59 -0400

    feat(tones): add tones

--- src/lib/constants/pads.ts
+++ src/lib/constants/pads.ts
@@ -1,16 +1,27 @@
 import padA from "@audios/pads/pad-a.mp3";
+import padASharp from "@audios/pads/pad-a-sharp.mp3";
+import padB from "@audios/pads/pad-b.mp3";
+import padG from "@audios/pads/pad-g.mp3";
+import padGSharp from "@audios/pads/pad-g-sharp.mp3";
+import padFSharp from "@audios/pads/pad-f-sharp.mp3";
+import padF from "@audios/pads/pad-f.mp3";
+import padE from "@audios/pads/pad-e.mp3";
+import padD from "@audios/pads/pad-d.mp3";
+import padDSharp from "@audios/pads/pad-d-sharp.mp3";
+import padCSharp from "@audios/pads/pad-c-sharp.mp3";
+import padC from "@audios/pads/pad-c.mp3";
 
 export const padsContinuos = {
-  C: "",
-  "C#": "",
-  D: "",
-  "D#": "",
-  E: "",
-  F: "",
-  "F#": "",
-  G: "",
-  "G#": "",
+  C: padC,
+  "C#": padCSharp,
+  D: padD,
+  "D#": padDSharp,
+  E: padE,
+  F: padF,
+  "F#": padFSharp,
+  G: padG,
+  "G#": padGSharp,
   A: padA,
-  "A#": "",
-  B: "",
+  "A#": padASharp,
+  B: padB,
 } as const;
